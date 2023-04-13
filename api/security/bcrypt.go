89ce5a90cd26a2d9f26137cce1698eacf7b6baca
Rafael Caixeta <caixetadev@gmail.com>
2023-04-13 15:01:53 -0300

    feat: add function to compare password

--- api/security/bcrypt.go
+++ api/security/bcrypt.go
@@ -6,3 +6,7 @@ import "golang.org/x/crypto/bcrypt"
 func Hash(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
+
+func VerifyPassword(passwordHash, passowrd string) error {
+	return bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(passowrd))
+}
