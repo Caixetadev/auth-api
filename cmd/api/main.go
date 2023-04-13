44767468067c169d879401d4385e578849be1808
Rafael Caixeta <caixetadev@gmail.com>
2023-04-13 00:52:35 -0300

    feat: initialize app

---
+++ cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"auth-api/api/models"
+	"auth-api/api/routes"
+
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	v := validator.New()
+
+	e.Validator = &models.UserValidator{Validator: v}
+
+	routes.InitAuthRoutes(e)
+
+	e.Logger.Fatal(e.Start(":3333"))
+}
