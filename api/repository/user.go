019345bfec59d635283745b62fae14b9b1b40cc9
Rafael Caixeta <caixetadev@gmail.com>
2023-04-13 15:01:38 -0300

    feat: create a method to get a user by email

--- api/repository/user.go
+++ api/repository/user.go
@@ -17,3 +17,23 @@ func (u User) Create(user models.User) error {
 	_, err := u.db.Exec("INSERT INTO users (name, lastname, email, password) VALUES ($1, $2, $3, $4)", user.Name, user.LastName, user.Email, user.Password)
 	return err
 }
+
+func (u User) GetUserByEmail(email string) (models.User, error) {
+	line, err := u.db.Query("SELECT id, password FROM users WHERE email = $1", email)
+
+	if err != nil {
+		return models.User{}, err
+	}
+
+	defer line.Close()
+
+	var user models.User
+
+	if line.Next() {
+		if err = line.Scan(&user.ID, &user.Password); err != nil {
+			return models.User{}, err
+		}
+	}
+
+	return user, nil
+}
