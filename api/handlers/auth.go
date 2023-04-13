5c1d026adf626925247e323a5220fb9eed8a6aad
Rafael Caixeta <caixetadev@gmail.com>
2023-04-13 00:51:25 -0300

    feat: add handler to register user

---
+++ api/handlers/auth.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"auth-api/api/db"
+	"auth-api/api/models"
+	"auth-api/api/repository"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func Register(c echo.Context) error {
+	var user models.User
+
+	if err := c.Bind(&user); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := c.Validate(user); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	db, err := db.Connect()
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	defer db.Close()
+
+	repository := repository.NewRepositoryOfAuth(db)
+
+	if err = repository.Create(user); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusCreated, user)
+}
