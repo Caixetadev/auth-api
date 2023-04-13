bd862d37c97d0d8f0e15551e7c787cd74ce26001
Rafael Caixeta <caixetadev@gmail.com>
2023-04-13 20:07:50 -0300

    feat: create function to get id of token

--- api/auth/token.go
+++ api/auth/token.go
@@ -1,9 +1,14 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
 )
 
 func CreateToken(userID uint64) (string, error) {
@@ -16,4 +21,45 @@ func CreateToken(userID uint64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, perm)
 
 	return token.SignedString([]byte("minhatestesupersecreta"))
+}
+
+// GetUserID return the userId that is saved in token
+func GetUserByID(c echo.Context) (uint64, error) {
+	tokenString := getToken(c)
+
+	token, err := jwt.Parse(tokenString, returnVerificationKey)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if permissions, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		userID, err := strconv.ParseUint(fmt.Sprintf("%.0f", permissions["userID"]), 10, 64)
+
+		if err != nil {
+			return 0, err
+		}
+
+		return userID, nil
+	}
+
+	return 0, errors.New("Invalid token")
+}
+
+func getToken(c echo.Context) string {
+	token := c.Request().Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func returnVerificationKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected signature method! %s", token.Header["alg"])
+	}
+
+	return []byte("minhatestesupersecreta"), nil
 }
