3f94565826bda48834f07220f1853558d9035ab3
Rafael Caixeta <caixetadev@gmail.com>
2023-04-17 20:38:52 -0300

    feat: add script sql

---
+++ sql/sql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
